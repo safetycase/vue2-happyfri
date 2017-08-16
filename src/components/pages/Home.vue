c58977e8e9a468facc1bb28ef5ca2e08e46f9b5c
safetycase <h_gao@sohu.com>
2017-08-17 06:37:38 +0800

    home中加图片按钮

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,8 +1,10 @@
 <template>
 	<div>
     	<MyHeader :title = 'title'></MyHeader>
-		<div>home_logo</div>
-		<router-link to="item">开始</router-link>
+    	<div>
+    		<div class="home_logo item_container_style"></div>
+    		<router-link to="item" class="start button_style" ></router-link>
+    	</div>
 	</div>
 </template>
 
@@ -23,5 +25,31 @@
 </script>
 
 <style scoped>
-
+	.item_container_style{
+		height: 11.625rem;
+		width: 13.15rem;
+		background-repeat: no-repeat;
+		position: absolute;
+		top: 4.1rem;
+		left: 1rem;
+	}
+	.home_logo{
+		background-image: url(../../assets/images/1-2.png);
+		background-size: 13.142rem 100%;
+		background-position: right center;
+	}
+	.button_style{
+        display: block;
+        height: 2.1rem;
+        width: 4.35rem;
+        background-size: 100% 100%;
+        position: absolute;
+        top: 16.5rem;
+        left: 50%;
+        margin-left: -2.4rem;
+        background-repeat: no-repeat;
+	}
+	.start{
+        background-image: url(../../assets/images/1-4.png);
+    }
 </style>
