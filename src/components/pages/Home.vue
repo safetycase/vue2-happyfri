9f90f80ef813ce3bf34f6682da348916d650a213
safetycase <h_gao@sohu.com>
2017-08-16 21:48:51 +0800

    新建Home.vue

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<header>
+			<span>{{title}}</span>
+		</header>
+		<div>home_logo</div>
+		<router-link to="item">开始</router-link>
+	</div>
+</template>
+
+<script>
+  	export default {
+		name: 'Home',
+		data(){
+			return {
+				title: '第一周'
+			}
+		}   
+  	}
+
+</script>
+
+<style>
+  	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import Home from '@/components/pages/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      // path: '/',
-      // name: 'Hello',
-      // component: Hello
+       path: '/',
+       name: 'Home',
+       component: Home
     }
   ]
 })
