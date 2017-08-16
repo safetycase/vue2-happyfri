f8414d894ccd00e28171578cf8a313ca768e38d0
safetycase <h_gao@sohu.com>
2017-08-16 21:02:24 +0800

    项目新建

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-	<div>
-    	<router-view></router-view>
-    </div>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-
-  	export default {
-    
-  	}
-
+export default {
+  name: 'app'
+}
 </script>
 
-<style lang="less">
-  	
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import routes from './router/router'
-import store from './store/'
-import ajax from './config/ajax'
-import './style/common'
-import './config/rem'
+import App from './App'
+import router from './router'
 
-Vue.use(VueRouter)
-const router = new VueRouter({
-	routes
-})
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
-	router,
-	store,
-}).$mount('#app')
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
