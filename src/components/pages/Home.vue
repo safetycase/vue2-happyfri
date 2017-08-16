95c75ca52897e93910d5d15b74b2cfed96f5c2b7
safetycase <h_gao@sohu.com>
2017-08-17 06:11:34 +0800

    Home.vue中加入header，现实第一周

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
-		<header>
-			<span>{{title}}</span>
-		</header>
+    	<header class="top_tips">
+    		<span class="num_tip">{{title}}</span>
+    	</header>
 		<div>home_logo</div>
 		<router-link to="item">开始</router-link>
 	</div>
@@ -20,6 +20,28 @@
 
 </script>
 
-<style>
-  	
+<style scoped>
+  	.top_tips{
+		position: absolute;
+		height: 7.35rem;
+		width: 3.25rem;
+		top: -1.3rem;
+		right: 1.6rem;
+		background: url(../../assets/images/WechatIMG2.png) no-repeat;
+		background-size: 100% 100%;
+		z-index: 10;
+
+	}
+	.num_tip{
+		position: absolute;
+		left: 0.48rem;
+		bottom: 1.1rem;
+		height: 0.7rem;
+		width: 2.5rem;
+		font-size: 0.6rem;
+		font-family: '黑体';
+		font-weight: 600;
+		color: #a57c50;
+		text-align: center;
+	}
 </style>
