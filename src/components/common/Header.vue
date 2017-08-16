9d4c091fb9143cbafbbee42bbf4cf83c2ddc14a6
safetycase <h_gao@sohu.com>
2017-08-17 06:30:07 +0800

    独立Header.vue

---
+++ src/components/common/Header.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+     	<header class="top_tips">
+    		<span class="num_tip">{{title}}</span>
+    	</header>
+	</div>
+</template>
+
+<script>
+  	export default {
+		props: ['title']   
+  	}
+
+</script>
+
+<style>
+  	.top_tips{
+		position: absolute;
+		height: 7.35rem;
+		width: 3.25rem;
+		top: -1.3rem;
+		right: 1.6rem;
+		background: url(../../assets/images/WechatIMG2.png) no-repeat;
+		background-size: 100% 100%;
+		z-index: 10;
+
+	}
+	.num_tip{
+		position: absolute;
+		left: 0.48rem;
+		bottom: 1.1rem;
+		height: 0.7rem;
+		width: 2.5rem;
+		font-size: 0.6rem;
+		font-family: '黑体';
+		font-weight: 600;
+		color: #a57c50;
+		text-align: center;
+	}
+</style>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,16 +1,18 @@
 <template>
 	<div>
-    	<header class="top_tips">
-    		<span class="num_tip">{{title}}</span>
-    	</header>
+    	<MyHeader :title = 'title'></MyHeader>
 		<div>home_logo</div>
 		<router-link to="item">开始</router-link>
 	</div>
 </template>
 
 <script>
+	import MyHeader from '../common/Header.vue'
   	export default {
 		name: 'Home',
+		components: {
+			MyHeader
+		},
 		data(){
 			return {
 				title: '第一周'
@@ -21,27 +23,5 @@
 </script>
 
 <style scoped>
-  	.top_tips{
-		position: absolute;
-		height: 7.35rem;
-		width: 3.25rem;
-		top: -1.3rem;
-		right: 1.6rem;
-		background: url(../../assets/images/WechatIMG2.png) no-repeat;
-		background-size: 100% 100%;
-		z-index: 10;
 
-	}
-	.num_tip{
-		position: absolute;
-		left: 0.48rem;
-		bottom: 1.1rem;
-		height: 0.7rem;
-		width: 2.5rem;
-		font-size: 0.6rem;
-		font-family: '黑体';
-		font-weight: 600;
-		color: #a57c50;
-		text-align: center;
-	}
 </style>
