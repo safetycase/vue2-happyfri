c2fea6262ba4ee4f5b8678e770c8b1f245731aa3
safetycase <h_gao@sohu.com>
2017-08-17 06:52:06 +0800

    新建Item.vue

---
+++ src/components/pages/Item.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+    	<MyHeader :title = 'title'></MyHeader>
+    	<div>
+
+    	</div>
+	</div>
+</template>
+
+<script>
+	import MyHeader from '../common/Header.vue'
+  	export default {
+		name: 'Item',
+		components: {
+			MyHeader
+		},
+		data(){
+			return {
+				title: '第一题'
+			}
+		}   
+  	}
+
+</script>
+
+<style scoped>
+
+</style>
